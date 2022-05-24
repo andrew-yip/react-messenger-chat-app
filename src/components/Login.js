bdfeb458f1f6de87f6dcc0107010668055b9810b
Andrew <andrew.yip22@gmail.com>
2022-05-23 20:51:58 -1000

    added a login component

---
+++ src/components/Login.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { GoogleOutlined, FacebookOutlined } from '@ant-design/icons';
+
+const Login = () => {
+    return (
+        <div id="login-page">
+            <div id="login-card">
+                <h2>Welcome to Messenger Chat!</h2>
+                <div className='login-button google'>
+                    <GoogleOutlined /> Sign In with Google
+                    <br /> <br />
+                    <FacebookOutlined /> Sign In with Facebook
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Login; 
