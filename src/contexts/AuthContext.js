a9a5ec8b9f17f18fe1462afab40a4de0f48bc87b
Andrew <andrew.yip22@gmail.com>
2022-05-28 23:46:28 -1000

    modified/added changes to AuthContext file

--- src/contexts/AuthContext.js
+++ src/contexts/AuthContext.js
@@ -2,6 +2,30 @@ import React, { useContext, useState, useEffect } from 'react';
 import { useHistory } from 'react-router-dom';
 import { auth } from '../firebase';
 
+// creating the context
 const AuthContext = React.createContext();
 
+// to grab that context
 export const useAuth = () => useContext(AuthContext);
+
+export const AuthProvider = ( {  children }) => {
+    const [loading, setLoading] = useState(true); // setting loading state
+    const [user, setUser] = useState({}); // setting user state
+    const history = useHistory();
+
+    useEffect(() => {
+        auth.onAuthStateChanged((user) => { // grabbing the user from firebase authentication
+            setUser(user);
+            setLoading(false);
+            history.push('/chats'); // using react router dom to push our applcation to '/chats'
+        })
+    }, [user, history]);
+
+    const value = { user };
+
+    return (
+        <AuthContext.Provider value={value}>
+            {!loading && children}
+        </AuthContext.Provider>
+    );
+}
