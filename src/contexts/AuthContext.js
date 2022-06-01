a2730cd3d3ccb8f12a480d9e0abcdbd63d501d9b
Andrew <andrew.yip22@gmail.com>
2022-06-01 01:00:39 -1000

    added a feature so that the page redirects to chats if logged in

--- src/contexts/AuthContext.js
+++ src/contexts/AuthContext.js
@@ -10,14 +10,17 @@ export const useAuth = () => useContext(AuthContext);
 
 export const AuthProvider = ( {  children }) => {
     const [loading, setLoading] = useState(true); // setting loading state
-    const [user, setUser] = useState({}); // setting user state
+    const [user, setUser] = useState(null); // setting user state
     const history = useHistory();
 
     useEffect(() => {
         auth.onAuthStateChanged((user) => { // grabbing the user from firebase authentication
             setUser(user);
             setLoading(false);
-            history.push('/chats'); // using react router dom to push our applcation to '/chats'
+
+            // only navigate to the chats if we have a user
+            if (user) history.push('/chats'); // using react router dom to push our applcation to '/chats'
+            
         })
     }, [user, history]);
 
