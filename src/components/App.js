a70e31c3c2a4ca327313082f42787f4393c90a98
Andrew <andrew.yip22@gmail.com>
2022-06-04 07:11:30 +0100

    firebase and facebook auth changes in the backend

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,6 @@
 import React from "react"
 
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom"
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 import { AuthProvider } from "../contexts/AuthContext"
 import Chats from "./Chats"
